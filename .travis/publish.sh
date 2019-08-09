6157b459ce7f151ca440605ab43db256443b4e9e
Enrico Secondulfo <enrico.secondulfo@fidesdigital.com>
2019-08-09 16:11:49 +0200

    ci: added publish script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,8 @@
 ##!/usr/bin/bash bash
 
-if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
-   ./gradlew -PbintrayUser=$BINTRAY_USER -PbintrayApiKey=$BINTRAY_API_KEY bintrayUpload
+if [ "$TRAVIS_TEST_RESULT" -eq 0 ]; then
+   echo "$TRAVIS_COMMIT_MESSAGE"
+   ./gradlew -PbintrayUser="$BINTRAY_USER" -PbintrayApiKey="$BINTRAY_API_KEY" bintrayUpload
 
    else
      echo "Test failed"
